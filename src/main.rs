9352029f5dbec477cdb0a1ea6a92d0b913feeb90
so-su <komiyama.masaki.53w@st.kyoto-u.ac.jp>
2023-04-21 05:09:12 +0900

    CompareNat2 おわり

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod compare_nat_1;
+mod compare_nat_2;
 mod nat;
 mod utility;
 
 fn main() {
-    let deriv = compare_nat_1::CompareNat1::derive_u8(2, 5);
+    let deriv = compare_nat_2::CompareNat2::derive_u8(2, 3);
     println!("{}", utility::indent(deriv.to_string()));
 }
